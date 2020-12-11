dc0b7d8c7e1294c84a049abd63364207df3f82f7
ZiyiZhu <zz2374@columbia.edu>
2020-12-11 01:45:17 -0500

    add return

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,15 @@
 import boto3
 import json
 
+ACCESS_HEADERS = {
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin" : "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"}
+
 def lambda_handler(event, context):
     print("Hello CodePipeline!")
+    return {
+        'statusCode': 200,
+        'headers': ACCESS_HEADERS,
+        'body': json.dumps('Hello From CodePipeline')
+    }
